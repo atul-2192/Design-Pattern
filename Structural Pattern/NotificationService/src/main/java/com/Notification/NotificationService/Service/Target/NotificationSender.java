f7b039616d5658a944b3bef9e7c0f38b5eb7fc20
Atul <73394652+atul-2192@users.noreply.github.com>
2025-08-14 22:40:10 +0530

    Delete Structural Pattern/NotificationService directory

--- Structural Pattern/NotificationService/src/main/java/com/Notification/NotificationService/Service/Target/NotificationSender.java
+++
@@ -1,7 +0,0 @@
-package com.Notification.NotificationService.Service.Target;
-
-import com.Notification.NotificationService.Model.CustomerDetails;
-
-public interface NotificationSender {
-    void sendNotification(CustomerDetails recipient, String message);
-}
